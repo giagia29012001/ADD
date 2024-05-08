fc145ab60171a54225ecedae108b064fcf6715f4
giagia29012001 <tranlinh27082001@gmail.com>
2024-05-08 13:42:58 +0700

    Update web2.py

--- web2.py
+++ web2.py
@@ -526,11 +526,10 @@ def main():
                                                         data_bhyt= find_point(x_min, y_min, x_max, y_max)
                                                         x_bhyt, y_bhyt = data_bhyt[0]['x'], data_bhyt[0]['y']
                           
-                                        st.write(x_bhyt)
-                                            
-
-    
-                                        
+                                        if x_qr > x_bhyt and y_qr < y_bhyt:
+                                            rotated_image = cropped_img.rotate(180, expand=True)
+                                            st.image(rotated_image, use_column_width=True)
+                      
                         else:
                             st.warning('Vui lòng cung cấp đầy đủ thông tin.')
 
