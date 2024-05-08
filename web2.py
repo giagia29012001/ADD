dbdcfcf65b7907c4086a3df2a4ec01eeb6e65b5e
giagia29012001 <tranlinh27082001@gmail.com>
2024-05-08 14:04:01 +0700

    Update web2.py

--- web2.py
+++ web2.py
@@ -539,7 +539,47 @@ def main():
                                             rotated_image = cropped_img.rotate(90, expand=True)
                                             
                                         st.image(rotated_image, use_column_width=True)
-                      
+                                    if os.path.exists('./runs'):
+                                        shutil.rmtree('./runs')
+                                            
+                                    results = get_prediction(rotated_image, model_inf)
+                                    results.save()
+
+                                    img_res = cv2.imread('./runs/detect/exp/image0.jpg')
+                                    if img_res is not None:
+
+                                        img_res = cv2.cvtColor(img_res, cv2.COLOR_BGR2RGB)
+                                        st.image(img_res, use_column_width=True)
+
+                                        df = results.pandas().xyxy[0]
+                                        del df['class']
+                                        st.write(df)
+                                        st.header("Thông tin trích xuất từ ảnh BHYT")
+
+                                        des = set()
+                                        for name_type in df['name']:
+                                            if name_type not in des:
+                                                            # Xử lý hiển thị mô tả cho từng loại bệnh
+                                                if name_type == 'id':
+                                                    id_rows = df[df['name'] == 'id']
+
+                                                    # Lặp qua từng hàng trong DataFrame với 'name_type' là 'id'
+                                                    for index, row in id_rows.iterrows():
+                                                        x_min, y_min, x_max, y_max = row['xmin'], row['ymin'], row['xmax'], row['ymax']
+                                                            # Cắt ảnh
+                                                        cropped_img = rotated_image.crop((x_min, y_min, x_max, y_max))
+                                                        border_size = 10
+                                                        # Thêm viền đen cho hình ảnh
+                                                        cropped_img = ImageOps.expand(cropped_img, border=border_size, fill='white')
+                                                        image=cropped_img
+
+                                                        # Trích xuất văn bản sử dụng pytesseract
+                                                        text = pytesseract.image_to_string(image)
+                                                        text_id="MÃ BHYT: "+text
+                                                        # Hiển thị kết quả
+                                                        st.write(text_id)
+
+                        
                         else:
                             st.warning('Vui lòng cung cấp đầy đủ thông tin.')
 
